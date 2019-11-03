d50211f45f315c4c92b97b31ee75c718b3c5f9cd
Chris Eller <cteller@bsu.edu>
2019-11-03 15:23:14 -0500

    Create GUI forms and classes

---
+++ src/main/java/MainMenuFrame.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class MainMenuFrame {
+    private JButton clockInOutButton;
+    private JButton viewEmployeeInformationButton;
+    private JButton addNewEmployeeButton;
+}
