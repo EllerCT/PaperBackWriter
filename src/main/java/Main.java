7beb8c5bfe9659411bec7347fa3b405068d99080
Chris Eller <cteller@bsu.edu>
2019-11-03 18:47:40 -0500

    Make Main Menu Display

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args){
-
+        UserInterfaceController ui = new UserInterfaceController();
+        ui.showMenu();
+        
     }
 }
--- src/main/java/UserInterfaceController.java
+++ src/main/java/UserInterfaceController.java
@@ -1,2 +1,11 @@
+import javax.swing.*;
+
 public class UserInterfaceController {
+    public void showMenu(){
+        JFrame frame = new JFrame("MainMenuFrame");
+        frame.setContentPane(new MainMenuFrame().getPanel());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
