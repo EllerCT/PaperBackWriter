f70ae6cc916cc1673cae20672a3c783939693e13
Chris Eller <cteller@bsu.edu>
2019-11-03 17:02:20 -0500

    Create Clock Points window

---
+++ src/main/java/ClockPointsFrame.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class ClockPointsFrame {
+    private JPanel clockPointsPanel;
+    private JPanel entryPanel;
+    private JPanel buttonAreaPanel;
+    private JButton confirmButton;
+    private JTextField pinField;
+    private JComboBox eventCombo;
+}
