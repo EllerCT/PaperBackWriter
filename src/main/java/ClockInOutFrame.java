24f8c29150d44b3cc1ab1f659d5315321d9f6b0b
Chris Eller <cteller@bsu.edu>
2019-11-03 15:23:14 -0500

    Create GUI forms and classes

---
+++ src/main/java/ClockInOutFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ClockInOutFrame {
+    private JTextField pinField;
+    private JButton clockInOutButton;
+    private JLabel pinLabel;
+    private JPanel clockWindow;
+}
