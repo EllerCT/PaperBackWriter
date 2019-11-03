97d2becce31738fbefbfce1392639f8d30dcd35d
Chris Eller <cteller@bsu.edu>
2019-11-03 16:33:53 -0500

    Create Event Management form

---
+++ src/main/java/ManageEventsFrame.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class ManageEventsFrame {
+    private JPanel manageEventsPanel;
+    private JButton confirmButton;
+    private JButton cancelButton;
+    private JTable eventsTable;
+}
