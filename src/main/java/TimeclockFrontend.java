df37ff748af5e0b006b85f83c5d0cdc3bcab260a
Chris Eller <cteller@bsu.edu>
2019-11-08 17:44:48 -0500

    Create Timeclock Frontend interface

---
+++ src/main/java/TimeclockFrontend.java
@@ -0,0 +1,9 @@
+public interface TimeclockFrontend {
+    void mainMenu();
+    void timeClock();
+    void addEmployee();
+    void attendEvent();
+    void viewEmployees();
+    void manageEvents();
+    void getConfirmation();
+}
