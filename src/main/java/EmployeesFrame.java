d21cad91cdd33f3009fe28caed10437a927558e2
Chris Eller <cteller@bsu.edu>
2019-11-08 16:56:54 -0500

    Replace EmployeesDialog with EmployeesFrame

---
+++ src/main/java/EmployeesFrame.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class EmployeesFrame {
+    private JPanel employeesPanel;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+    private JTable employeeTable;
+}
