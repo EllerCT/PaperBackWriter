92e4d4627e0c16b85a8e0aad0d7f45d7b0510cfe
Chris Eller <cteller@bsu.edu>
2019-11-08 22:18:14 -0500

    Create EmployeeIO skeleton

---
+++ src/main/java/EmployeeIO.java
@@ -0,0 +1,29 @@
+public class EmployeeIO {
+    private IOSystem ioSystem;
+
+    public EmployeeIO(IOSystem ioSystem){
+        this.ioSystem = ioSystem;
+    }
+
+    public boolean hasNextEmployee() {
+        //TODO: implement hasNextEmployee
+        return false;
+    }
+
+    public Employee getNextEmployee() {
+        //TODO: implement getNextEmployee
+        return new Employee();
+    }
+
+    public void addNew(Employee employee) {
+        //TODO: implement addNew
+    }
+
+    public void remove(Employee employee) {
+        //TODO: implement remove
+    }
+
+    public void update(Employee employee) {
+        //TODO: implement update
+    }
+}
