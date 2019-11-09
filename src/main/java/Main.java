db04ccb3662c0631a5467db2ca5ee4e0b5868d96
Chris Eller <cteller@bsu.edu>
2019-11-08 20:29:25 -0500

    Refactor to instance TimeclockInterface

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args){
-        UserInterfaceController uic = new UserInterfaceController();
+        TimeclockFrontend uic = new UserInterfaceController();
         uic.mainMenu();
     }
 }
