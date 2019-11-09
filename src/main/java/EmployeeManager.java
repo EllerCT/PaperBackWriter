262a292f82c161b029d48ab1899bc26edd268fd5
Chris Eller <cteller@bsu.edu>
2019-11-08 22:13:38 -0500

    Define EmployeeManager

--- src/main/java/EmployeeManager.java
+++ src/main/java/EmployeeManager.java
@@ -1,2 +1,43 @@
+import java.util.*;
+
 public class EmployeeManager {
+    private EmployeeIO employeeIO;
+    private Map<PinNumber,Employee> employeeMap;
+
+    public EmployeeManager(){
+        employeeMap = new HashMap<PinNumber, Employee>();
+    }
+
+    public void loadEmployees(){
+        while (employeeIO.hasNextEmployee()) {
+            Employee nextEmployee = employeeIO.getNextEmployee();
+            employeeMap.put(nextEmployee.getPin(), nextEmployee);
+        }
+    }
+
+    public Map<PinNumber, Employee> getEmployeeMap(){
+        return employeeMap;
+    }
+
+    public void newEmployee(Employee employee){
+        employeeIO.addNew(employee);
+        employeeMap.put(employee.getPin(), employee);
+    }
+
+    public void removeEmployee(Employee employee){
+        employeeIO.remove(employee);
+        employeeMap.remove(employee.getPin());
+    }
+
+    public void updateEmployee(Employee employee){
+        employeeIO.update(employee);
+        employeeMap.put(employee.getPin(), employee);
+    }
+
+    public void changeEmployeePin(Employee employee, PinNumber pin){
+        removeEmployee(employee);
+        employee.setPin(pin);
+        newEmployee(employee);
+    }
+
 }
