394779fc88468ada6f8bcdd15780392db1bbb998
Chris Eller <cteller@bsu.edu>
2019-11-08 21:31:27 -0500

    Add setLocation to IOSystem interface

--- src/main/java/IOSystem.java
+++ src/main/java/IOSystem.java
@@ -1,7 +1,9 @@
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URI;
 
 public interface IOSystem {
+    void setLocation(URI location);
     void write(OutputStream out);
     InputStream read();
 }
