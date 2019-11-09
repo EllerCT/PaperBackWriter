f3d5e1204ad287f8c1a3d55ca98789b6209ea7b0
Chris Eller <cteller@bsu.edu>
2019-11-08 21:07:59 -0500

    Define Employee basic traits

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,2 +1,40 @@
+import java.time.Duration;
+
 public class Employee {
+    private String name;
+    private Duration hours;
+    private int points;
+    private PinNumber pin;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Duration getHours() {
+        return hours;
+    }
+
+    public void setHours(Duration hours) {
+        this.hours = hours;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public PinNumber getPin() {
+        return pin;
+    }
+
+    public void setPin(PinNumber pin) {
+        this.pin = pin;
+    }
 }
