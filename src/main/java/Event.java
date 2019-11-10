8b94f190f7b7522c6ccedff3b9f2d324997f6056
Chris Eller <cteller@bsu.edu>
2019-11-10 11:34:44 -0500

    Define Event

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,2 +1,17 @@
 public class Event {
+    private String eventCode;
+    private int pointWorth;
+
+    public Event(String code, int points){
+        this.eventCode = code;
+        this.pointWorth = points;
+    }
+
+    public String getEventCode() {
+        return eventCode;
+    }
+
+    public int getPointWorth() {
+        return pointWorth;
+    }
 }
