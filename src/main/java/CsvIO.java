fe783e5ffd203167e8c3b02006d31a7d47d1d45c
Chris Eller <cteller@bsu.edu>
2019-11-10 11:40:33 -0500

    Create CsvIO

---
+++ src/main/java/CsvIO.java
@@ -0,0 +1,21 @@
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
+
+public class CsvIO implements IOSystem{
+
+    @Override
+    public void setLocation(URI location) {
+
+    }
+
+    @Override
+    public void write(OutputStream out) {
+
+    }
+
+    @Override
+    public InputStream read() {
+        return null;
+    }
+}
