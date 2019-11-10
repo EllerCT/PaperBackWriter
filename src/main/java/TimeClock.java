e5069eb70fa797a39662e55a51574c5ff3f8ed92
Michael A. Gallegos <magallegos56@gmail.com>
2019-11-10 15:24:37 -0500

    Set up TimeClock class.

--- src/main/java/TimeClock.java
+++ src/main/java/TimeClock.java
@@ -1,2 +1,15 @@
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class TimeClock {
+
+    public void clockIn(Employee employee){
+        employee.setClockInTime(LocalDateTime.now());
+    }
+
+    public void clockOut(Employee employee){
+        Duration hoursWorked = Duration.between(employee.getClockInTime(),LocalDateTime.now());
+        employee.setHours(hoursWorked);
+        employee.setClockInTime(null);
+    }
 }
