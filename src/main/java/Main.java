271157da5fe9db85d31f4efa3780159f0b92212b
Chris Eller <cteller@bsu.edu>
2019-11-12 20:32:27 -0500

    Make temporary very ugly main method run

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,30 @@
-import javax.swing.*;
+import io_pipes.EmployeeIOPipe;
+import io_systems.IOSystem;
+import io_systems.LocalFileIOSystem;
+import managers.EmployeeManager;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
 
 public class Main {
     public static void main(String[] args){
-        TimeclockFrontend uic = new UserInterfaceController();
+        UserInterfaceController uic = new UserInterfaceController();
+        EmployeeManager employeeManager = new EmployeeManager();
+        IOSystem employeeIOS = new LocalFileIOSystem();
+        File hackyNewFile = new File("EmployeeCSV.csv");
+        if (!hackyNewFile.exists()) {
+            try {
+                hackyNewFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        URI fileLocation = hackyNewFile.toURI();
+        employeeIOS.setLocation(fileLocation);
+        EmployeeIOPipe pipe = new EmployeeIOPipe(employeeIOS);
+        employeeManager.setEmployeeIOPipe(pipe);
+        uic.setEmployeeManager(employeeManager);
         uic.mainMenu();
     }
 }
