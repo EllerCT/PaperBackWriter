87efe1db0ea82615afa620d524307de887a0c2e3
Chris Eller <cteller@bsu.edu>
2019-12-26 11:51:38 -0500

    Add new Main Menu frame

---
+++ src/main/java/swing_frames/MainMenuFrame.java
@@ -0,0 +1,9 @@
+package swing_frames;
+
+import javax.swing.*;
+
+public class MainMenuFrame {
+    private JButton employeeButton;
+    private JButton productButton;
+    private JPanel contentPane;
+}
