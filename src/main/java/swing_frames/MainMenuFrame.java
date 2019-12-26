77b4882b1338afb2147e30405d09d925a73c4973
Chris Eller <cteller@bsu.edu>
2019-12-26 12:10:35 -0500

    Add action setters and a content pane getter

--- src/main/java/swing_frames/MainMenuFrame.java
+++ src/main/java/swing_frames/MainMenuFrame.java
@@ -1,9 +1,22 @@
 package swing_frames;
 
 import javax.swing.*;
+import java.awt.event.ActionListener;
 
 public class MainMenuFrame {
     private JButton employeeButton;
     private JButton productButton;
     private JPanel contentPane;
+
+    public void setEmployeeButtonAction(ActionListener action) {
+        employeeButton.addActionListener(action);
+    }
+
+    public void setProductButtonAction(ActionListener action) {
+        productButton.addActionListener(action);
+    }
+
+    public JPanel getContentPane() {
+        return contentPane;
+    }
 }
