5634fcbb16a66b403162541b34c394976adb17bf
Chris Eller <cteller@bsu.edu>
2020-01-03 14:57:15 -0500

    Implement Resource fields

--- src/main/java/data_structures/Resource.java
+++ src/main/java/data_structures/Resource.java
@@ -1,4 +1,61 @@
 package data_structures;
 
+import java.util.Objects;
+
 public class Resource {
+    private ResourceType type;
+    private String unitSize;
+    private double pricePerUnit;
+    private String name;
+
+    public Resource(String name) {
+        this.name = name;
+        this.unitSize = "";
+        this.pricePerUnit = 0.00;
+    }
+
+    public ResourceType getType() {
+        return type;
+    }
+
+    public void setType(ResourceType type) {
+        this.type = type;
+    }
+
+    public String getUnitSize() {
+        return unitSize;
+    }
+
+    public void setUnitSize(String unitSize) {
+        this.unitSize = unitSize;
+    }
+
+    public double getPricePerUnit() {
+        return pricePerUnit;
+    }
+
+    public void setPricePerUnit(double pricePerUnit) {
+        this.pricePerUnit = pricePerUnit;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + unitSize + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Resource resource = (Resource) o;
+        return Double.compare(resource.pricePerUnit, pricePerUnit) == 0 &&
+                type == resource.type &&
+                unitSize.equals(resource.unitSize) &&
+                name.equals(resource.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, unitSize, pricePerUnit, name);
+    }
 }
