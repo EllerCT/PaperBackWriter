c3e4f8c76e99c117c6b7723ed358f5b202bb6e13
Chris Eller <cteller@bsu.edu>
2020-01-03 13:52:49 -0500

    Create resources window

---
+++ src/main/java/swing_frames/ResourcesFrame.java
@@ -0,0 +1,48 @@
+package swing_frames;
+
+import javax.swing.*;
+import javax.swing.table.TableModel;
+import java.awt.event.ActionListener;
+
+public class ResourcesFrame {
+    private JPanel contentPane;
+    private JTable resourceTable;
+    private JButton saveButton;
+    private JButton cancelButton;
+    private JButton addButton;
+    private JButton removeButton;
+
+    public JPanel getPanel() {
+        return this.contentPane;
+    }
+
+    public void setSaveButtonListener(ActionListener action) {
+        saveButton.addActionListener(action);
+    }
+
+    public void setCancelButtonListener(ActionListener action) {
+        cancelButton.addActionListener(action);
+    }
+
+    public void setAddButtonListener(ActionListener action) {
+        addButton.addActionListener(action);
+    }
+
+    public void setRemoveButtonListener(ActionListener action) {
+        removeButton.addActionListener(action);
+    }
+
+    public void setTableModel(TableModel model) {
+        resourceTable.setModel(model);
+    }
+
+    public TableModel getModel() {
+        return resourceTable.getModel();
+    }
+
+    public JTable getTable() {
+        return resourceTable;
+    }
+
+
+}
