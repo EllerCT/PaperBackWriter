c5e1434b6d23fb086273d81f6d1bd5de334cec98
Chris Eller <cteller@bsu.edu>
2020-01-04 20:05:14 -0500

    Add a products main menu

---
+++ src/main/java/swing_frames/ProductsMenuFrame.java
@@ -0,0 +1,27 @@
+package swing_frames;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class ProductsMenuFrame {
+    private JPanel contentPane;
+    private JButton costAnalysisButton;
+    private JButton resourcesButton;
+    private JButton browseProductsButton;
+
+    public JPanel getPane() {
+        return contentPane;
+    }
+
+    public void setCostAnalysisButtonListener(ActionListener action) {
+        costAnalysisButton.addActionListener(action);
+    }
+
+    public void setResourcesButtonListener(ActionListener action) {
+        resourcesButton.addActionListener(action);
+    }
+
+    public void setBrowseProductsButtonListener(ActionListener action) {
+        browseProductsButton.addActionListener(action);
+    }
+}
