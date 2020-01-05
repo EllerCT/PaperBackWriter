c65645194b0bfc436d0367567a51bf8091638736
Chris Eller <cteller@bsu.edu>
2020-01-05 14:40:37 -0500

    Add ResourceManager to user interface controller launch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
 import io_pipes.EmployeeIOPipe;
 import io_pipes.EventIOPipe;
+import io_pipes.ResourceIOPipe;
 import io_systems.IOSystem;
 import io_systems.LocalFileIOSystem;
 import managers.EmployeeManager;
 import managers.EventManager;
+import managers.ResourceManager;
 
 import java.io.File;
 
@@ -11,25 +13,33 @@ public class Main {
     public static void main(String[] args){
         EmployeeManager employeeManager = new EmployeeManager();
         EventManager eventManager = new EventManager();
+        ResourceManager resourceManager = new ResourceManager();
 
         IOSystem employeeIOS = new LocalFileIOSystem();
         IOSystem eventIOS = new LocalFileIOSystem();
+        IOSystem resourceIOS = new LocalFileIOSystem();
 
         File employeeFile = new File("EmployeeCSV.csv");
         File eventsFile = new File("EventCSV.csv");
+        File resourcesFile = new File("Resources.csv");
 
         employeeIOS.setLocation(employeeFile.toURI());
         eventIOS.setLocation(eventsFile.toURI());
+        resourceIOS.setLocation(resourcesFile.toURI());
+
 
         EmployeeIOPipe employeePipe = new EmployeeIOPipe(employeeIOS);
         EventIOPipe eventPipe = new EventIOPipe(eventIOS);
+        ResourceIOPipe resourcePipe = new ResourceIOPipe(resourceIOS);
 
         employeeManager.setEmployeeIOPipe(employeePipe);
         eventManager.setEventIOPipe(eventPipe);
+        resourceManager.setResourceIOPipe(resourcePipe);
 
         UserInterfaceController uic = new UserInterfaceController();
         uic.setEmployeeManager(employeeManager);
         uic.setEventManager(eventManager);
+        uic.setResourceManager(resourceManager);
         uic.mainMenu();
     }
 }
