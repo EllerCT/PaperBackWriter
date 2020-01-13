3c4c1e0a630b5a0e45198d432c32c0ac54f331a5
Chris Eller <cteller@bsu.edu>
2020-01-13 11:13:18 -0500

    Implement ProductManager

--- src/main/java/managers/ProductManager.java
+++ src/main/java/managers/ProductManager.java
@@ -1,4 +1,57 @@
 package managers;
 
+import data_structures.Product;
+import io_pipes.ProductIOPipe;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ProductManager {
+
+    private ProductIOPipe productIOPipe;
+    private HashMap<String, Product> productMap;
+
+    public ProductManager() {
+        this.productMap = new HashMap<>();
+    }
+
+    public void fetchProducts() {
+        productMap.clear();
+        productMap.putAll(productIOPipe.loadProducts());
+    }
+
+    public void storeProducts() {
+        productIOPipe.saveProducts(productMap);
+    }
+
+    public void setProductIOPipe(ProductIOPipe pipe) {
+        this.productIOPipe = pipe;
+    }
+
+    public Map<String, Product> getProductMap() {
+        return productMap;
+    }
+
+    public void setProductMap(HashMap<String, Product> map) {
+        this.productMap = map;
+    }
+
+    public void newProduct(Product newProduct) {
+        String key = Product.generateKeyFor(newProduct);
+        if (!productMap.containsKey(key)) {
+            productMap.put(key, newProduct);
+        }
+    }
+
+    public void removeProduct(Product toBeRemoved) {
+        String key = Product.generateKeyFor(toBeRemoved);
+        productMap.remove(key);
+    }
+
+    public void updateProduct(Product toBeUpdated) {
+        String key = Product.generateKeyFor(toBeUpdated);
+        if (productMap.containsKey(key)) {
+            productMap.put(key, toBeUpdated);
+        }
+    }
 }
