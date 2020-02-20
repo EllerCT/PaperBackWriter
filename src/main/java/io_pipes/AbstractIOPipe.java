680c7662938c35c777635e4a5e3e6277c1402a08
Chris Eller <cteller@bsu.edu>
2020-02-19 22:42:35 -0500

    Prepare AbstractIOPipe for extension

--- src/main/java/io_pipes/AbstractIOPipe.java
+++ src/main/java/io_pipes/AbstractIOPipe.java
@@ -1,26 +1,74 @@
 package io_pipes;
 
+import io_systems.IOSystem;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 //TODO: Generify IOPipe load/saves.
 public abstract class AbstractIOPipe implements IOPipe {
+
+    protected static CSVFormat CSV_FORMAT;
+    protected IOSystem ioSystem;
+
+    public AbstractIOPipe(IOSystem ioSystem) {
+        this.ioSystem = ioSystem;
+    }
+
     @Override
     public Map load() {
-
-        return null;
+        try {
+            InputStreamReader isr = new InputStreamReader(ioSystem.read());
+            CSVParser csvParser = new CSVParser(isr, CSV_FORMAT.withSkipHeaderRecord());
+            HashMap<Object, Object> map = new HashMap<>();
+            List<CSVRecord> records = csvParser.getRecords();
+            if (records.size() > 0) {
+                for (CSVRecord record : records) {
+                    Object translation = translateFromRecord(record);
+                    map.put(keyMakerFor(translation), translation);
+                }
+            }
+            csvParser.close();
+            return map;
+        } catch (IOException e) {
+            System.err.println(e.getLocalizedMessage());
+            return new HashMap<>();
+        }
     }
 
     @Override
     public void save(Map map) {
-
+        try {
+            ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(byteStream));
+            CSVPrinter printer = new CSVPrinter(writer, CSV_FORMAT);
+            for (Object entry : map.values()) {
+                translateToRecord(printer, entry);
+            }
+            printer.flush();
+            byte[] outBytes = byteStream.toByteArray();
+            printer.close();
+            ioSystem.write(outBytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    protected abstract Object translateRecord(CSVRecord record, Object into);
+    protected abstract Object keyMakerFor(Object object);
 
-    protected void makeNewFile() throws IOException {
+    protected abstract void setCsvFormat(CSVFormat format);
+
+    protected abstract Object translateFromRecord(CSVRecord record);
+
+    protected abstract void translateToRecord(CSVPrinter printer, Object object);
+
+    protected void makeNewFile(String name) throws IOException {
 
     }
 }
