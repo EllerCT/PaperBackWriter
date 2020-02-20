f6fa2b0db40c135a217d4a8d8e8a14ff89c167a4
Chris Eller <cteller@bsu.edu>
2020-02-20 18:39:52 -0500

    Add tabbed panes and toolbars

--- src/main/java/swing_frames/DesktopFrame.java
+++ src/main/java/swing_frames/DesktopFrame.java
@@ -5,6 +5,16 @@ import javax.swing.*;
 public class DesktopFrame extends JFrame {
     private JPanel contentPane;
     private JDesktopPane desktopPane;
+    private JTabbedPane tabbedPane1;
+    private JToolBar employeeToolbar;
+    private JToolBar productToolbar;
+    private JButton clockInOutButton;
+    private JButton attendanceButton;
+    private JButton employeesButton;
+    private JButton eventsButton;
+    private JButton costAnalysisButton;
+    private JButton productBrowserButton;
+    private JButton resourcesButton;
 
     public DesktopFrame() {
         desktopPane.setLayout(null);
