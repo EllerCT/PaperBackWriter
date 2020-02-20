e776987b73eb7c7f8289472afcf45dcf0c2a2db1
Chris Eller <cteller@bsu.edu>
2020-02-20 14:25:11 -0500

    Generify Managers into an AbstractManager

---
+++ src/main/java/managers/AbstractManager.java
@@ -0,0 +1,37 @@
+package managers;
+
+import io_pipes.IOPipe;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class AbstractManager {
+    protected IOPipe pipe;
+    protected Map<Object, Object> map;
+
+    public AbstractManager() {
+        this.map = new HashMap<>();
+    }
+
+    public void fetch() {
+        this.map.putAll(pipe.load());
+    }
+
+    public void store() {
+        this.pipe.save(map);
+    }
+
+    public void setIOPipe(IOPipe pipe) {
+        this.pipe = pipe;
+    }
+
+    public Map<Object, Object> getMap() {
+        return this.map;
+    }
+
+    public abstract void addNew(Object object);
+
+    public abstract void remove(Object object);
+
+    public abstract void update(Object object);
+}
