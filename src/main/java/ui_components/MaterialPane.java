52ec66d3ecab8c64912d0805a30d83abc00b0921
Chris Eller <cteller@bsu.edu>
2020-02-19 20:07:10 -0500

    Add capability for automatic updating

--- src/main/java/ui_components/MaterialPane.java
+++ src/main/java/ui_components/MaterialPane.java
@@ -3,9 +3,11 @@ package ui_components;
 import data_structures.Material;
 import data_structures.Resource;
 import data_structures.ResourceType;
+import listeners.cost_analysis.UpdatePriceListener;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class MaterialPane extends JPanel {
     private final int PREFERRED_HEIGHT = 50;
@@ -18,7 +20,7 @@ public class MaterialPane extends JPanel {
 
     public MaterialPane() {
         this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-        this.setPreferredSize(new Dimension(450, PREFERRED_HEIGHT));
+        this.setPreferredSize(new Dimension(500, PREFERRED_HEIGHT));
         this.setMaximumSize(new Dimension(Integer.MAX_VALUE, PREFERRED_HEIGHT));
         typeOfMaterial = new JComboBox<>(ResourceType.values());
         configureTypeOfMaterialBox();
@@ -37,6 +39,31 @@ public class MaterialPane extends JPanel {
         this.add(removeMaterial);
     }
 
+    public JComboBox<ResourceType> getTypeOfMaterial() {
+        return typeOfMaterial;
+    }
+
+    public JComboBox<Resource> getSpecificMaterial() {
+        return specificMaterial;
+    }
+
+    public JFormattedTextField getNumberOfMaterial() {
+        return numberOfMaterial;
+    }
+
+    public JFormattedTextField getPriceOfMaterial() {
+        return priceOfMaterial;
+    }
+
+    public void setMaterialTypeChangedAction(ActionListener listener) {
+        typeOfMaterial.addActionListener(listener);
+    }
+
+    public void setUpdatePriceAction(UpdatePriceListener listener) {
+        specificMaterial.addActionListener(listener);
+        numberOfMaterial.addPropertyChangeListener("value", listener);
+    }
+
     private void configureTypeOfMaterialBox() {
         typeOfMaterial.setPreferredSize(new Dimension(100, PREFERRED_HEIGHT));
     }
@@ -44,15 +71,16 @@ public class MaterialPane extends JPanel {
     private void configureSpecificMaterialBox() {
         specificMaterial.setPreferredSize(new Dimension(150, PREFERRED_HEIGHT));
         specificMaterial.addItem(new Resource("None"));
+
     }
 
     private void configureNumberOfMaterialField() {
         numberOfMaterial.setPreferredSize(new Dimension(60, PREFERRED_HEIGHT));
         numberOfMaterial.setMaximumSize(new Dimension(80, PREFERRED_HEIGHT));
         numberOfMaterial.setHorizontalAlignment(JTextField.CENTER);
+        numberOfMaterial.setFocusLostBehavior(JFormattedTextField.COMMIT_OR_REVERT);
         // Format the text field to Integers via Integer.valueOf
         numberOfMaterial.setValue(Integer.valueOf(0));
-
     }
 
     private void configurePriceOfMaterial() {
@@ -72,6 +100,9 @@ public class MaterialPane extends JPanel {
             parent.repaint();
         });
     }
+
+    // TODO: Figure out how to generify this to a Controller
+
 
     public Material getMaterial() {
         if (!specificMaterial.getSelectedItem().equals(new Resource("None"))) {
