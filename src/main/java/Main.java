490c7d409a94c7dba320bcd3f0a445a0fbe83b9f
Chris Eller <cteller@bsu.edu>
2020-03-20 12:57:22 -0400

    Remove Iteration 4 update functionality

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,9 @@
 import utilities.Initializer;
-import utilities.Updater;
 
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args){
-        // Temporary, for I4 only.
-        for (String arg : args) {
-            if (arg.contains("update")) {
-                new Updater().updateProductFile();
-            }
-        }
         SwingUtilities.invokeLater(new Initializer());
     }
 }
